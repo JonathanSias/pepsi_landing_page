2cce59fbd0079588aaf801eb48131b6d54d0fbd3
Jonathan Sias <jcsias@inf.ufpel.edu.br>
2020-09-01 13:14:24 -0300

    landing Pode Ser

---
+++ main.js
@@ -0,0 +1,8 @@
+function changeColor(whatever) {
+    document.querySelector(".pepsi").src = whatever;
+}
+
+function changeBgColor(color) {
+    const sec = document.querySelector(".sec");
+    sec.style.background = color;
+}
